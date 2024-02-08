a322376fe1122890feab167bfecebc5fedc40758
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-02-08 16:04:44 -0300

    Deleção de nota

--- src/app.tsx
+++ src/app.tsx
@@ -41,6 +41,15 @@ export function App() {
         setSearch(query);
     }
 
+    function onNoteDeleted(id: string) {
+        const notesArray = notes.filter(note => {
+            return note.id !== id;
+        });
+
+        setNotes(notesArray);
+        localStorage.setItem('notes', JSON.stringify(notesArray));
+    }
+
     const filteredNotes =
         search !== ""
             ? notes.filter((note) =>
@@ -51,7 +60,7 @@ export function App() {
             : notes;
 
     return (
-        <div className="max-w-6xl mx-auto my-12 space-y-6">
+        <div className="max-w-6xl mx-auto my-12 space-y-6 px-5">
             <img src={logo} alt="NLW Expert" />
             <form className="w-full">
                 <input
@@ -63,10 +72,10 @@ export function App() {
             </form>
             <div className="h-px bg-slate-700" />
 
-            <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
                 <NewNoteCard onNoteCreated={onNoteCreated} />
                 {filteredNotes.map((note) => {
-                    return <NoteCard key={note.id} note={note} />;
+                    return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />;
                 })}
             </div>
         </div>
